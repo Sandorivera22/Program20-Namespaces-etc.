13d7edc75a008667304d1de5d0fa6fd903d5a606
Sandorivera <gabrielriveraarn05@gmail.com>
2025-01-07 16:55:52 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+namespace NameSpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            File.WriteAllText(@"C:\Leccion 22-Excepciones\Example.txt", reply);
+            Console.ReadLine();
+        }
+    }
+}
